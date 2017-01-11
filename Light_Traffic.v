b256d010e8c7bf2545346b9c4cd2b378fb05f222
Ali Yazdi <ma.yazdi75@gmail.com>
2017-01-11 15:50:12 +0330

    end module

--- Light_Traffic.v
+++ Light_Traffic.v
@@ -33,4 +33,37 @@ module A_Green_Counter(Rst,En,Clk,Finished,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[
 
 endmodule 
 
-module 
+module B_Green_Counter(Rst,En,Clk, Finished,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
+
+    input Rst,En,Clk;
+    output Finished,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
+
+endmodule
+
+module Check_Traffic(End_Period,Clk,A_Traffic,B_Traffic, Exsi_Traffic,Count,Finished);
+
+    input End_Period,Clk,A_Traffic,B_Traffic,Finished;
+    output Count,Exsi_Traffic;
+
+endmodule
+
+module Check_Traffic_Counter(Rst,En,Finished);
+    
+    input Rst,En;
+    output Finished;
+
+endmodule
+
+module Light_Wink(Exsi_Traffic,Count,Finished,A_Light,B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
+
+    input Exsi_Traffic,Finished;
+    output Count,A_Light,B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
+
+endmodule
+
+module Light_Wink_Counter(Rst,En,Finished);
+    
+    input Rst,En;
+    output Finished;
+
+endmodule
