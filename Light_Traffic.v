67b6a125fde15c7e4f791a7d12b4d18eeb7e71ff
Ali Yazdi <ma.yazdi75@gmail.com>
2017-01-11 14:59:33 +0330

    adding module

--- Light_Traffic.v
+++ Light_Traffic.v
@@ -15,8 +15,22 @@ endmodule
 module PO_Handle(Enable_PO_Handle,A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
 
     input Enable_PO_Handle;
-    output A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
+    output A_Light, B_Light, A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
 
 endmodule
 
-module Light_Auto(Enable_Auto,)
+module Light_Auto(Enable_Auto, Clk, A_Traffic, B_Traffic, A_Light, B_Light, Count, Finished, Exsi_Traffic);
+    
+    input Enable_Auto, Clk, A_Traffic, B_Traffic, Finished, Exsi_Traffic;
+    output A_Light, B_Light, Count;
+
+endmodule
+
+module A_Green_Counter(Rst,En,Clk,Finished,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
+
+    input Rst,En,Clk;
+    output Finished,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
+
+endmodule 
+
+module 
