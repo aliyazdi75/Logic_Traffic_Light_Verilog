b8ff3f4586a70389eb94ae18b1b7855cc4830c0d
Ali Yazdi <ma.yazdi75@gmail.com>
2017-01-11 16:35:44 +0330

    ok PO_Handle

--- Light_Traffic.v
+++ Light_Traffic.v
@@ -6,20 +6,30 @@ module Light_Traffic (A, B, R, Clk, A_Traffic, B_Traffic);
     wire Enable_PO_Handle;
     wire Enable_Auto;
 
-    assign Enable_PO_Handle=A|B;
+    assign Enable_PO_Handle=A|B&~R;
     assign Enable_Auto=~Enable_PO_Handle;
 
-    PO_Handle poh= PO_Handle(Enable_PO_Handle,A,B,R,A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
+    PO_Handle poh= PO_Handle(Enable_PO_Handle,A,B,A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
     Light_Auto la=Light_Auto(Enable_Auto,Clk, A_Traffic, B_Traffic, A_Light, B_Light,1,1)
 
 endmodule
 
-module PO_Handle(Enable_PO_Handle,A,B,R,A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
+module PO_Handle(Enable_PO_Handle,A,B,A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
 
     input Enable_PO_Handle;
     output A_Light, B_Light, A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
-
-
+    if (Enable_PO_Handle) begin
+        assign A_Light=A;
+        assign B_Light=B;
+        assign A_Time_L[0]=1;
+        assign A_Time_L[1]=1;
+        assign A_Time_L[2]=1;
+        assign A_Time_L[3]=1;
+        assign B_Time_L[0]=1;
+        assign B_Time_L[1]=1;
+        assign B_Time_L[2]=1;
+        assign B_Time_L[3]=1;
+    end
 
 endmodule
 
