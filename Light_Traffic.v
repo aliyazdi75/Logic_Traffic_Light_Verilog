7c37c6dc55a708bfa07c745ff3183bf66a3deecc
Ali Yazdi <ma.yazdi75@gmail.com>
2017-01-16 00:34:27 +0330

    Rewrite code:) For more information look ate the last commit code in more state machine write code from desine of state oriject and create counter and main for handle them.

--- Light_Traffic.v
+++ Light_Traffic.v
@@ -1,107 +1,86 @@
 
-module Light_Traffic (A, B, R, Clk, A_Traffic, B_Traffic,A_Light, B_Light,A_Time_L,A_Time_H,B_Time_L,B_Time_H);
+module Light_Traffic (A, B, R, Clk, A_Traffic, B_Traffic,count,finished,first_both_red,A_Light, B_Light,A_Time_L,A_Time_H,B_Time_L,B_Time_H);
 
-    input A, B, R, Clk, A_Traffic, B_Traffic;
-    output A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
-
-    wire Enable_PO_Handle;
-    wire Enable_Auto;
-
-    assign Enable_PO_Handle=A|B&~R;
-    assign Enable_Auto=~Enable_PO_Handle;
-
-    PO_Handle poh= PO_Handle(Enable_PO_Handle,A,B,A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
-    Light_Auto la=Light_Auto(Enable_Auto,Clk, A_Traffic, B_Traffic, A_Light, B_Light,A_Time_L[3:0],
-                                                        A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0],1,0,0,0,1);
-
-endmodule
-
-module PO_Handle(Enable_PO_Handle,A,B,A_Light, B_Light,A_Time_L,A_Time_H,B_Time_L,B_Time_H);
-
-    input Enable_PO_Handle;
-    output A_Light, B_Light, A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
-    if (Enable_PO_Handle) begin
-        assign A_Light=A;
-        assign B_Light=B;
-        assign A_Time_L[0]=1;
-        assign A_Time_L[1]=1;
-        assign A_Time_L[2]=1;
-        assign A_Time_L[3]=1;
-        assign B_Time_L[0]=1;
-        assign B_Time_L[1]=1;
-        assign B_Time_L[2]=1;
-        assign B_Time_L[3]=1;
-    end
-
-endmodule
-
-module Light_Auto(Enable_Auto, Clk, A_Traffic, B_Traffic, A_Light, B_Light, Count_A,Count_B,Count_Both,Finished, Exist_Traffic);
+    input A, B, R, Clk, A_Traffic, B_Traffic,finished,first_both_red;
+    output count,A_Light, B_Light,[3:0] A_Time_L,[3:0] A_Time_H,[3:0] B_Time_L,[3:0] B_Time_H;
+    reg [3:0] state;
+    parameter S0 = 3'b100, S1 = 3'b101, S2 = 3'b000, S3 = 3'b001, S4=3'b010, S5=3'b011, //Auto
+                 S6=3'b100, S7=3'b010, //Police_Handle
+                 S8=3'b010, S9=3'b011, S10=3'b000, S11=3'b001; //Wink
     
-    input Enable_Auto, Clk, A_Traffic, B_Traffic,Count_A,Count_B,Count_Both,Finished, Exist_Traffic;
-    output A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
-
-    if (Enable_Auto)begin
-        if (Exist_Traffic)begin
-            A_Green_Counter(~Count_A,Count_A,A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
-            B_Green_Counter(~Count_B,Count_B,A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
-            if(Finished)begin
-                Count_Both=1;
-                Both_Red(~Count_Both,Count_Both,A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
-            end
+    always @ (posedge Clk or negedge R)
+        if (~R) state = S0; //Initialize to state S0
+        else if(~A & ~B)
+            case (state)
+                S0: state = S1;
+                S1: if(finished) state = S2;
+                S2: state = S3;
+                S3: if(finished) begin
+                        if(~first_both_red) 
+                            state = S4;
+                        else begin
+                            if(~(A_Traffic & B_Traffic))
+                                state=S8;
+                            else
+                                state = S0;
+                        end
+                    end
+                S4: state=S5;
+                S5: if(finished) state=S2;
+                //5s Check
+                //assign output
+                S8: state=S9;
+                S9: if(finished) state=S10;
+                S10: state=S11;
+                S11: if(finished) state=S8;
+            endcase
+        else begin
+            if(A)
+                state=S6;
+            else if(B)
+                state=S7;
         end
-    end
+
+    assign #10 AB = state[2:1];
+    assign #10 count= state[0];
 
 endmodule
 
-module Both_Red(Rst,En,Clk,A_Light, B_Light,A_Time_L,A_Time_H,B_Time_L,B_Time_H);
-    
-    input Rst,En,Clk;
-    output A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
-
+module counter_A (En,Q,CLK,RST);
+    input En,CLK,RST;
+    output [7:0]Q;
+    reg [7:0] num;
+    always @ (posedge CLK)
+        if (~RST) state = 9'b0;
+        else
+            if (En)
+                num=num+1;
+    assign #10 Q = num;
 endmodule
 
-module A_Green_Counter(Rst,En,Clk,A_Light, B_Light,A_Time_L,A_Time_H,B_Time_L,B_Time_H);
-
-    input Rst,En,Clk;
-    output A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
-
-    if(En)begin
-        
-    end
-
-endmodule 
-
-module B_Green_Counter(Rst,En,Clk,A_Light, B_Light,A_Time_L,A_Time_H,B_Time_L,B_Time_H);
-
-    input Rst,En,Clk;
-    output A_Light, B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
-
+module counter_B (En,Q,CLK,RST);
+    input En,CLK,RST;
+    output [7:0]Q;
+    reg [7:0] num;
+    always @ (posedge CLK)
+        if (~RST) state = 9'b0;
+        else
+            if (En)
+                num=num+1;
+    assign #10 Q = num;
 endmodule
 
-module Check_Traffic(End_Period,Clk,A_Traffic,B_Traffic, Exist_Traffic,Count,Finished);
-
-    input End_Period,Clk,A_Traffic,B_Traffic,Finished;
-    output Count,Exist_Traffic;
-
-endmodule
-
-module Check_Traffic_Counter(Rst,En,Finished);
-    
-    input Rst,En;
-    output Finished;
-
-endmodule
-
-module Light_Wink(Exist_Traffic,Count,Finished,A_Light,B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0]);
-
-    input Exist_Traffic,Finished;
-    output Count,A_Light,B_Light,A_Time_L[3:0],A_Time_H[3:0],B_Time_L[3:0],B_Time_H[3:0];
-
-endmodule
-
-module Light_Wink_Counter(Rst,En,Finished);
-    
-    input Rst,En;
-    output Finished;
-
+module main(A, B, R, Clk, A_Traffic, B_Traffic,A_Light, B_Light,A_Time_L,A_Time_H,B_Time_L,B_Time_H);
+   
+    input A, B, R, Clk, A_Traffic, B_Traffic;
+    output A_Light, B_Light,[3:0] A_Time_L,[3:0] A_Time_H,[3:0] B_Time_L,[3:0] B_Time_H;                                                                                                                                                                                       
+    reg count, finished;
+    wire [15:0]Q;
+    //5s Check
+    //assign output
+    //wich use of cnt 5 or 95?
+    //solve first_both_red,A_Light for pass
+    Light_Traffic (A, B, R, Clk, A_Traffic, B_Traffic,count,finished,first_both_red,A_Light, B_Light,A_Time_L,A_Time_H,B_Time_L,B_Time_H);
+    counter8 my_counter(En,Q,CLK,RST);
+    and a(finished,Q[2], Q[1], Q[0]);
 endmodule
